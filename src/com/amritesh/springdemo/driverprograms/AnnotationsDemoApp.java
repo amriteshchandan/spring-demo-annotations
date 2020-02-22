3f128f2eeac07239204916076f71c7abc0d22400
amriteshchandan <amriteshchandan@yahoo.in>
2020-02-22 17:59:18 +0530

    Ioc Using Annotations - @Component

---
+++ src/com/amritesh/springdemo/driverprograms/AnnotationsDemoApp.java
@@ -0,0 +1,16 @@
+package com.amritesh.springdemo.driverprograms;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.amritesh.springdemo.Coach;
+
+public class AnnotationsDemoApp {
+
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		Coach coach = context.getBean("thatSillyCoach", Coach.class);
+		System.out.println(coach.getDailyWorkout());
+		context.close();
+	}
+
+}
