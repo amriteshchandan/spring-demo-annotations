6cf09c9ffc7e3e589cb07e7a097ae04ca6c01745
amriteshchandan <amriteshchandan@yahoo.in>
2020-02-22 18:38:06 +0530

    DI Using Annotations - @Autowired

--- src/com/amritesh/springdemo/TennisCoach.java
+++ src/com/amritesh/springdemo/TennisCoach.java
@@ -1,13 +1,27 @@
 package com.amritesh.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
 
+	private FortuneService fortuneService;
+	
+	@Autowired
+	TennisCoach(FortuneService fortuneService) {
+		System.out.println("[TennisCoach] constructor");
+		this.fortuneService = fortuneService;
+	}
+	
 	@Override
 	public String getDailyWorkout() {
 		return "Practice your backhand volley.";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune();
 	}
 
 }
--- src/com/amritesh/springdemo/driverprograms/AnnotationsDemoApp.java
+++ src/com/amritesh/springdemo/driverprograms/AnnotationsDemoApp.java
@@ -10,6 +10,7 @@ public class AnnotationsDemoApp {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		Coach coach = context.getBean("tennisCoach", Coach.class);
 		System.out.println(coach.getDailyWorkout());
+		System.out.println(coach.getDailyFortune());
 		context.close();
 	}
 
