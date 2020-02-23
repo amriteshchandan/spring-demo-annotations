76e59873a2d8b29abff05e91cb0bab8774cce4da
amriteshchandan <amriteshchandan@yahoo.in>
2020-02-23 23:01:58 +0530

    DI Using Annotations - Lifecycle methods using @PostConstruct and @PreDestroy

---
+++ src/com/amritesh/springdemo/driverprograms/AnnotationsBeanScopeDemoApp.java
@@ -0,0 +1,19 @@
+package com.amritesh.springdemo.driverprograms;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.amritesh.springdemo.Coach;
+
+public class AnnotationsBeanScopeDemoApp {
+
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		Coach coach1 = context.getBean("tennisCoach", Coach.class);
+		Coach coach2 = context.getBean("tennisCoach", Coach.class);
+		System.out.println(coach1);
+		System.out.println(coach2);
+		System.out.println("Pointing to same object :: " + (coach1 == coach2));
+		context.close();
+	}
+
+}
