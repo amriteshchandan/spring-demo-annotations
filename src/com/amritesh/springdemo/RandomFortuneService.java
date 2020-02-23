19858610343223a0e634382a4fda71f13501e3aa
amriteshchandan <amriteshchandan@yahoo.in>
2020-02-23 21:32:58 +0530

    DI Using Annotations - Using @Qualifier - Part 2

--- src/com/amritesh/springdemo/RandomFortuneService.java
+++ src/com/amritesh/springdemo/RandomFortuneService.java
@@ -1,14 +1,21 @@
 package com.amritesh.springdemo;
 
+import java.util.Random;
+
 import org.springframework.stereotype.Component;
 
 @Component
 public class RandomFortuneService implements FortuneService {
 
+	private String[] fortunes = {
+			"Luck 1", "Luck 2", "Luck 3", "Luck 4", "Luck 5"
+	};
+	
+	private Random myRandom = new Random();
+	
 	@Override
 	public String getFortune() {
-		// TODO Auto-generated method stub
-		return null;
+		return fortunes[myRandom.nextInt(fortunes.length)];
 	}
 
 }
